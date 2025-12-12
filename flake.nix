eb1097d2baee2f223bc637760de92312520bab14
Matej Urbas <matej.urbas@gmail.com>
2025-12-12 11:21:27 +0000

    migrate to pyproject.toml and ruff

--- flake.nix
+++ flake.nix
@@ -2,41 +2,67 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system: with nixpkgs.legacyPackages.${system};
-    let
-      checkInputs = with python3.pkgs; [
-        pytestCheckHook
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
+      system:
+      with nixpkgs.legacyPackages.${system};
+      let
+        pyproject = pkgs.lib.importTOML ./pyproject.toml;
+        pkg = python3Packages.buildPythonPackage {
+          pname = pyproject.project.name;
+          version = pyproject.project.version;
+          src = self;
+          format = "pyproject";
 
-      propagatedBuildInputs = with python3.pkgs; [
-        click
-        prometheus-client
-        pyyaml
-      ];
+          propagatedBuildInputs = with python3Packages; [
+            click
+            prometheus-client
+            pyyaml
+          ];
 
-      devInputs = with python3.pkgs; [
-        black
-        flake8
-        isort
-        nodePackages.prettier
-        nodePackages.pyright
-        pip
-        pkgs.parallel
-        pytest
-        pytest-cov
-        pytest-watch
-        types-pyyaml
-      ];
+          checkInputs = with python3Packages; [
+            pytestCheckHook
+          ];
 
-      pkg = python3.pkgs.buildPythonApplication {
-        pname = "file-time-exporter";
-        version = "local";
-        src = pkgs.nix-gitignore.gitignoreSource [] ./.;
-        inherit checkInputs propagatedBuildInputs;
-        pytestFlagsArray = ["-v" "tests"];
-      };
-    in {
-      packages.default = pkg;
-      devShells.default = pkg.overrideAttrs(oldAttrs: { nativeBuildInputs = oldAttrs.nativeBuildInputs ++ devInputs; });
-    });
+          pytestFlagsArray = [
+            "-v"
+            "tests"
+          ];
+
+          nativeBuildInputs = with python3Packages; [
+            setuptools
+          ];
+        };
+
+        pkg-editable = python3.pkgs.mkPythonEditablePackage {
+          pname = pyproject.project.name;
+          inherit (pyproject.project) scripts version;
+          root = ".";
+        };
+      in
+      {
+        packages.default = pkg;
+        devShells.default = mkShell {
+          packages = [
+            nixfmt-rfc-style
+            nodePackages.prettier
+            parallel
+            pkg-editable
+            prek
+            pyright
+            ruff
+            treefmt
+          ];
+          inputsFrom = [
+            pkg
+          ];
+        };
+      }
+    );
 }
